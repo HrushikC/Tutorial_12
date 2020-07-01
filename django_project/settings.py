0523eef16a40f6e67f3d98c96f375ec7c94a9b9c
ashwinchar <charashwin@gmail.com>
2020-07-01 17:41:38 -0400

    switched from sqlite3 to postgresql

--- django_project/settings.py
+++ django_project/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tutor_site',
+        'USER': 'postgres',
+        'PASSWORD': 'ash6051!',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
